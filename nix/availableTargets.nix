86ccc10fb4500bdc9bc2b7d8b317f5418bbd2f06
Vincent Lafeychine <vincent.lafeychine@proton.me>
2026-01-30 11:30:21 +0100

    chore(nix): Simplify syntax of flake.nix

--- nix/availableTargets.nix
+++ nix/availableTargets.nix
@@ -2,8 +2,8 @@
   pkgs,
   rust-overlay,
 }: desiredComponents:
-with pkgs.lib;
-let
+with pkgs.lib; let
   toolchain = rust-overlay.lib._internal.defaultManifests.nightly.latest.pkg;
   getArch = pkg: attrNames toolchain.${pkg}.target;
-in foldl' intersectLists (getArch (head desiredComponents)) (map getArch (tail desiredComponents))
+in
+  foldl' intersectLists (getArch (head desiredComponents)) (map getArch (tail desiredComponents))
