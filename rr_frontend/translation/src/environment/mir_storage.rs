6766d121d1fc5209c8614580ac89b967560702f1
Vincent Lafeychine <vincent.lafeychine@proton.me>
2026-03-05 18:16:00 +0100

    fix(rustc): Use Mutex instead of thread_local

--- rr_frontend/translation/src/environment/mir_storage.rs
+++ rr_frontend/translation/src/environment/mir_storage.rs
@@ -6,37 +6,40 @@
 //! Because of the [stealing mechanism], retrieving MIR bodies with borrowck facts can panic if the bodies are
 //! previously stolen.
 //!
-//! Thread local storage requires all data stored in it to have a `'static` lifetime.
+//! The mutex storage requires all data stored in it to have a `'static` lifetime.
 //! Therefore, we transmute the lifetime `'tcx` away when storing the data. To ensure that nothing gets
 //! meessed up, we require the client to provide a witness: an instance of type `TyCtxt<'tcx>` that is used to
 //! show that the lifetime that the client provided is indeed `'tcx`.
 //!
-//! See also: https://doc.rust-lang.org/nightly/nightly-rustc/rustc_borrowck/consumers/fn.get_bodies_with_borrowck_facts.html
+//! See also: <https://doc.rust-lang.org/nightly/nightly-rustc/rustc_borrowck/consumers/fn.get_bodies_with_borrowck_facts.html>
 //!
 //! [stealing mechanism]: https://rustc-dev-guide.rust-lang.org/mir/passes.html#stealing
 
-use std::cell::RefCell;
 use std::collections::HashMap;
-use std::{mem, thread_local};
+use std::mem;
+use std::sync::{LazyLock, Mutex};
 
 use rr_rustc_interface::borrowck::{self, consumers};
 use rr_rustc_interface::data_structures::fx::FxHashMap;
 use rr_rustc_interface::hir::def_id::LocalDefId;
-use rr_rustc_interface::middle::{queries, ty, util};
+use rr_rustc_interface::middle::{mir, queries, ty, util};
 
-thread_local! {
-    pub static MIR_BODIES:
-        RefCell<HashMap<LocalDefId, consumers::BodyWithBorrowckFacts<'static>>> =
-        RefCell::new(HashMap::new());
+pub(crate) struct BodyWithBorrowckFacts<'tcx> {
+    pub body: mir::Body<'tcx>,
+
+    /// Polonius input facts.
+    pub input_facts: Option<Box<consumers::PoloniusInput>>,
+
+    /// The table that maps Polonius points to locations in the table.
+    pub location_table: Option<consumers::PoloniusLocationTable>,
 }
+
+static MIR_FACTS: LazyLock<Mutex<HashMap<LocalDefId, BodyWithBorrowckFacts<'static>>>> =
+    LazyLock::new(|| Mutex::new(HashMap::new()));
 
 /// Provider for `mir_borrowck`
 ///
-/// See also: https://github.com/rust-lang/rust/blob/main/tests/ui-fulldeps/obtain-borrowck.rs
-///
-/// # Safety
-///
-/// See the module level comment.
+/// See also: <https://github.com/rust-lang/rust/blob/main/tests/ui-fulldeps/obtain-borrowck.rs>
 pub(crate) fn mir_borrowck(
     tcx: ty::TyCtxt<'_>,
     def_id: LocalDefId,
@@ -48,13 +51,23 @@ pub(crate) fn mir_borrowck(
     let bodies_with_facts: FxHashMap<LocalDefId, consumers::BodyWithBorrowckFacts<'static>> =
         unsafe { mem::transmute(bodies_with_facts) };
 
-    MIR_BODIES.with(|state| {
-        let mut map = state.borrow_mut();
-        for (def_id, body_with_facts) in bodies_with_facts {
-            let res = map.insert(def_id, body_with_facts);
-            assert!(res.is_none());
-        }
-    });
+    #[expect(clippy::iter_over_hash_type)]
+    for (def_id, body_with_facts) in bodies_with_facts {
+        let consumers::BodyWithBorrowckFacts {
+            body,
+            input_facts,
+            location_table,
+            ..
+        } = body_with_facts;
+
+        let res = MIR_FACTS.lock().unwrap().insert(def_id, BodyWithBorrowckFacts {
+            body,
+            input_facts,
+            location_table,
+        });
+
+        assert!(res.is_none());
+    }
 
     let mut providers = util::Providers::default();
     borrowck::provide(&mut providers.queries);
@@ -65,11 +78,8 @@ pub(crate) fn mir_borrowck(
 pub(crate) fn retrieve_mir_body<'tcx>(
     _tcx: ty::TyCtxt<'tcx>,
     def_id: LocalDefId,
-) -> Option<consumers::BodyWithBorrowckFacts<'tcx>> {
-    let body_with_facts: Option<consumers::BodyWithBorrowckFacts<'static>> = MIR_BODIES.with(|state| {
-        let mut map = state.borrow_mut();
-        map.remove(&def_id)
-    });
+) -> Option<BodyWithBorrowckFacts<'tcx>> {
+    let body_with_facts = MIR_FACTS.lock().unwrap().remove(&def_id);
 
     // SAFETY: For soundness we need to ensure that the bodies have the same lifetime (`'tcx`), which they had
     // before they were stored in the thread local.
